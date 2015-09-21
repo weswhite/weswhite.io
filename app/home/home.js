1942c0d9891398829fb854948d2a8fc3e78de22d
weswhite <wtealwhite@gmail.com>
2015-09-20 22:43:41 -0500

    adding scrollMagic

--- app/home/home.js
+++ app/home/home.js
@@ -1,3 +1,13 @@
 /**
  * Created by Wesley_T_White on 9/20/2015.
  */
+$(function() {
+    var controller = new ScrollMagic.Controller()
+});
+
+var scene = new ScrollMagic.Scene({
+    triggerElement: '#pinned-trigger1', // starting scene, when reaching this element
+    duration: 400 // pin the element for a total of 400px
+})
+    .setPin('#fifth-panel'); // the element we want to pin
+    .addTo(controller);
